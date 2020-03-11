d73fd48e469942086ce1bea8a1a7a6287319a543
Laraib Iqbal <621833@pdsb.net>
2020-03-10 21:28:44 -0400

    -lesson 10 files

--- 10 Parameters and Results/ParametersAndResults.playground/Pages/Exercise-Using Return Values.xcplaygroundpage/Contents.swift
+++ 10 Parameters and Results/ParametersAndResults.playground/Pages/Exercise-Using Return Values.xcplaygroundpage/Contents.swift
@@ -5,17 +5,15 @@
 
  The function `impossibleBeliefsCount` takes several numbers of reported unlikely incidents. It then prints the number of impossible things to be believed:
 */
-func impossibleBeliefsCount(pigsFlying: Int, frogsBecomingPrinces: Int, multipleLightningStrikes: Int) {
+func impossibleBeliefsCount(pigsFlying: Int, frogsBecomingPrinces: Int, multipleLightningStrikes: Int) -> Int {
     return pigsFlying + frogsBecomingPrinces + multipleLightningStrikes
 }
-let total = impossibleBeliefsCount
-print(total)
+let count = impossibleBeliefsCount(pigsFlying: 5, frogsBecomingPrinces: 6, multipleLightningStrikes: 4)
+//print(count)
 //: - callout(Exercise):Update the `impossibleBeliefsCount` function so that instead of printing the value, it returns it as an `Int`.
 //:
 //: `impossibleThingsPhrase` creates a phrase using string interpolation:
-func impossibleThingsPhrase() -> String {
-    let numberOfImpossibleThings = 10
-    let meal = "teatime"
+func impossibleThingsPhrase(numberOfImpossibleThings: Int, meal: String) -> String {
     return "Why, I've believed as many as \(numberOfImpossibleThings) before \(meal)"
 }
 //: - callout(Exercise): Update the `impossibleThingsPhrase` function so that, instead of using its two internal constants, it takes two arguments: `numberOfImpossibleThings` as an `Int` and `meal` as a `String`.
@@ -23,10 +21,11 @@ func impossibleThingsPhrase() -> String {
 //: Now you have two functions that take parameters and return values.
 //: - callout(Exercise): Call `impossibleBeliefsCount` and store the result in a constant.\
 //:Call `impossibleThingsPhrase`, passing in the result of `impossibleBeliefsCount` as one of the arguments.
+var impossible = impossibleThingsPhrase(numberOfImpossibleThings: 10, meal:"Pizza")
+print(impossible)
 
-
-
-
+impossible = impossibleThingsPhrase(numberOfImpossibleThings: count, meal: "Shawarma")
+print(impossible)
 
 
 //: [Previous](@previous)  |  page 15 of 17  |  [Next: Exercise: Argument Label](@next)
