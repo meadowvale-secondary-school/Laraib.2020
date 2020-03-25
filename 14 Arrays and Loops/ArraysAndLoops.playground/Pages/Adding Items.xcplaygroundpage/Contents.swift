1780a7916fafdfca08935cd442e7aceb576d99ba
Laraib Iqbal <621833@pdsb.net>
2020-03-25 15:06:23 -0400

    - Lesson 14

--- 14 Arrays and Loops/ArraysAndLoops.playground/Pages/Adding Items.xcplaygroundpage/Contents.swift
+++ 14 Arrays and Loops/ArraysAndLoops.playground/Pages/Adding Items.xcplaygroundpage/Contents.swift
@@ -5,13 +5,13 @@
  
  Remember from the Instances playground that a type followed by parentheses is how you create an instance of that type. To create a mutable empty array that will hold strings, do this:
 */
-var list = [String]()
+var emptyList = [String]()
 //: Once you've created the array, there are several ways to add items to it. You can add a single item using the `append` instance method:
-list.append("Banana")
+emptyList.append("Banana")
 //: You can add an item at a specific index using the `insert` instance method. As with everywhere you use an index, it has to be within the array or the program will crash:
-list.insert("Kumquat", at: 0)
+emptyList.insert("Kumquat", at: 0)
 //: You can append a whole array of items using the compound assignment operator `+=`:
-list += ["Strawberry", "Plum", "Watermelon"]
+emptyList += ["Strawberry", "Plum", "Watermelon"]
 //: - experiment: Practice adding items to the list using each of the three methods. Which do you prefer? When might you want to use each one?
 
 
